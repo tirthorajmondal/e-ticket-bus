41a9256640eccea44152f09a2263ab54c73c23d6
tirthoraj Mondal <tirtho.raj.2003@gmail.com>
2024-02-21 10:23:30 +0600

    ######

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// function seatStyle (){
+//     const seatId = document.addEventListener('click', function(event){
+//         event.style.backgroundColor = 'red';
+//     })
+// }
+
+
+function seatStyle (){
+    const seatId = document.getElementById('B1');
+    seatId.style.backgroundColor = '#1DD100';
+    seatId.style.color = 'white';
+}
+
+
+// function seatStyle (){
+//     const seats = document.getElementsByClassName('tickets');
+//     for(const seat of seats){
+
+//     }
+//     seat.style.backgroundColor = '#1DD100';
+//     seat.style.color = 'white';
+
+// }
